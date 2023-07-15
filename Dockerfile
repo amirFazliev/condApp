1be589aaefc817227d6d29536d30a1e065088896
Amir Fazliev <fazliev.amir@yandex.ru>
2023-07-15 08:34:09 +0300

    before tests commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:8-jdk-alpine
+
+EXPOSE 8081
+
+ADD build/libs/condApp-0.0.1-SNAPSHOT.jar myapp.jar
+
+ENTRYPOINT ["java","-jar","/myapp.jar"]
--- src/test/java/com/example/condapp/CondAppApplicationTests.java
+++ src/test/java/com/example/condapp/CondAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.condapp;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CondAppApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8080), String.class);
+        System.out.println(forEntity.getBody());
     }
 
 }
