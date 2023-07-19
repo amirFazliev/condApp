ce09e692e8ac43e35cb1e00ef69cad07fc71250b
Amir Fazliev <fazliev.amir@yandex.ru>
2023-07-19 09:00:30 +0300

    tests commit

--- src/main/java/com/example/condapp/config/JavaConfig.java
+++ src/main/java/com/example/condapp/config/JavaConfig.java
@@ -4,14 +4,17 @@ import com.example.condapp.profiles.DevProfile;
 import com.example.condapp.profiles.ProductionProfile;
 import com.example.condapp.profiles.SystemProfile;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 @Configuration
 public class JavaConfig {
 
+
     @Bean
-    @ConditionalOnProperty(prefix = "profile", value = "dev")
+    @ConditionalOnProperty(prefix = "profile", name = "dev")
     public SystemProfile devProfile() {
         return new DevProfile();
     }
--- src/test/java/com/example/condapp/CondAppApplicationTests.java
+++ src/test/java/com/example/condapp/CondAppApplicationTests.java
@@ -6,22 +6,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+
+//@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CondAppApplicationTests {
 
     @Autowired
-    TestRestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
+
+//    @Container
+    private static final GenericContainer<?> myAppDev = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+//    @Container
+    private static final GenericContainer<?> myAppProd = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
 
     @BeforeAll
     public static void setUp() {
-
+        myAppDev.start();
+        myAppProd.start();
     }
 
     @Test
     void contextLoads() {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8080), String.class);
-        System.out.println(forEntity.getBody());
+        ResponseEntity<String> forEntityMyAppDev = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/profile", String.class);
+        ResponseEntity<String> forEntityMyAppProd = restTemplate.getForEntity("http://localhost:" + myAppProd.getMappedPort(8081)+ "/profile", String.class);
+
+        System.out.println("MyAppDev: " + forEntityMyAppDev.getBody());
+        System.out.println("MyAppProd: " + forEntityMyAppProd.getBody());
+    }
+
+    @Test
+    public void devMessageTest() {
+        String expectedText = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/profile", String.class).getBody();
+
+        String resultText = "Current profile is dev";
+
+        assertThat(expectedText, resultText);
+    }
+
+    @Test
+    public void prodMessageTest() {
+        String expectedText = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8081) + "/profile", String.class).getBody();
+
+        String resultText = "Current profile is production";
+
+        assertThat(expectedText, resultText);
     }
 
 }
